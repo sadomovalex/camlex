105d12ee2e05f7393acf5723037395a86963a620
Alexey Sadomov <sadomovalex@camlex-online.org>
2009-12-24 20:31:53 +0000

    Add camlex project into codeplex svn

---
+++ Camlex.NET.ConsoleTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Camlex.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Camlex.Where(x => (string)x["Email"] == "test@example.com");
+        }
+    }
+}
