0de9c95416b377f6cb4fafe7f211747995c89208
sadomovalex <sadomovalex@localhost>
2009-12-26 21:48:14 +0000

    More tests

---
+++ Camlex.NET.UnitTests/CamlexTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Camlex.NET;
+
+namespace Camlex.NET.UnitTests
+{
+    [TestFixture]
+    public class CamlexTests
+    {
+        [Test]
+        public void test_THAT_single_eq_expression_IS_translated_sucessfully()
+        {
+            string caml = Camlex.Where(x => (string)x["Title"] == "testValue");
+
+            string expected =
+               @"<Where>
+                  <Eq>
+                     <FieldRef Name='Title' />
+                     <Value Type='Text'>testValue</Value>
+                  </Eq>
+               </Where>";
+
+            Assert.That(caml, Is.EqualTo(expected));
+        }
+    }
+}
