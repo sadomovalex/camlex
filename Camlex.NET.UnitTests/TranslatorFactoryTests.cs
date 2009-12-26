70e918b0f78c64006efd97a5a2fa9b2ea714cfcf
Alexey Sadomov <sadomovalex@camlex-online.org>
2009-12-26 21:48:14 +0000

    More tests

---
+++ Camlex.NET.UnitTests/TranslatorFactoryTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Camlex.NET.Interfaces;
+using NUnit.Framework;
+
+namespace Camlex.NET.UnitTests
+{
+    [TestFixture]
+    public class TranslatorFactoryTests
+    {
+        [Test]
+        public void test_WHEN_expression_is_equal_THEN_eq_translator_is_created()
+        {
+            var tr = new TranslatorFactory().Create(ExpressionType.Equal);
+            Assert.That(tr, Is.InstanceOf(typeof(EqTranslator)));
+        }
+    }
+}
