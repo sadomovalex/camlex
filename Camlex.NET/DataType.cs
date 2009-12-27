7ed1bd1ed879b83d3ff2f9569a86203b5a01ee47
Alexey Sadomov <sadomovalex@camlex-online.org>
2009-12-27 19:56:15 +0000

    Add available data types

---
+++ Camlex.NET/DataType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Camlex.NET
+{
+    public enum DataType
+    {
+        Attachments,
+        Boolean,
+        Choice,
+        Computed,
+        Counter,
+        DateTime,
+        Lookup,
+        ModStat,
+        MultiChoice,
+        Number,
+        Text,
+        User,
+        Note
+    }
+}
