ba6ab7b19cb3d6dffaccd71cd1aa707333df5592
sadomovalex <sadomovalex@localhost>
2009-12-27 08:22:37 +0000

    * IAnalyzer is added * EqAnalyzer is added * Common place for all exceptions and error messages is added * More tests

---
+++ Camlex.NET/Exceptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Microsoft.SharePoint;
+
+namespace Camlex.NET
+{
+    public class GenericException : Exception
+    {
+        public GenericException(string message, params object[] args) :
+            base(string.Format(message, args))
+        {}        
+    }
+
+    public class NonSupportedExpressionException : GenericException
+    {
+        public NonSupportedExpressionException(Expression<Func<SPItem, bool>> expr):
+            base(ErrorMessages.NON_SUPPORTED_EXPRESSION, expr)
+        {
+        }
+    }
+}
---
+++ Camlex.NET/Impl/Eq/EqAnalyzer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Camlex.NET.Interfaces;
+using Microsoft.SharePoint;
+
+namespace Camlex.NET.Impl.Eq
+{
+    public class EqAnalyzer : IAnalyzer
+    {
+        public bool IsValid(Expression<Func<SPItem, bool>> expr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IOperand GetLeftOperand(Expression<Func<SPItem, bool>> expr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IOperand GetRightOperand(Expression<Func<SPItem, bool>> expr)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
