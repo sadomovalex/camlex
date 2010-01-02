dd81271689d47a4244ee8b1e648e0a8b957c486b
sadomovalex <sadomovalex@localhost>
2010-01-02 10:57:35 +0000

    AnalyzerFactory added

--- Camlex.NET/Camlex.cs
+++ Camlex.NET/Camlex.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using Camlex.NET.Impl;
+using Camlex.NET.Impl.Factories;
 using Camlex.NET.Interfaces;
 using Microsoft.SharePoint;
 
@@ -12,11 +13,17 @@ namespace Camlex.NET
 {
     public static class Camlex
     {
-        public static ITranslatorFactory TranslatorFactory = new TranslatorFactory();
+        private static ITranslatorFactory translatorFactory;
+        static Camlex()
+        {
+            // factories setup
+            var analyzerFactory = new AnalyzerFactory();
+            translatorFactory = new TranslatorFactory(analyzerFactory);
+        }
 
         public static string Where(Expression<Func<SPItem, bool>> expr)
         {
-            var translator = TranslatorFactory.Create(expr.Body.NodeType);
+            var translator = translatorFactory.Create(expr.Body.NodeType);
             return translator.Translate(expr);
         }
     }
