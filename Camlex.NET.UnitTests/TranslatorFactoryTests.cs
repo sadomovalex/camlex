2091eefc63a6ce9223dc2c050df0b23016749e80
Alexey Sadomov <sadomovalex@camlex-online.org>
2010-01-02 11:18:51 +0000

    More tests

--- Camlex.NET.UnitTests/TranslatorFactoryTests.cs
+++ Camlex.NET.UnitTests/TranslatorFactoryTests.cs
@@ -5,18 +5,28 @@ using System.Linq.Expressions;
 using System.Text;
 using Camlex.NET.Impl;
 using Camlex.NET.Impl.Eq;
+using Camlex.NET.Impl.Factories;
+using Camlex.NET.Interfaces;
 using NUnit.Framework;
+using Rhino.Mocks;
 
 namespace Camlex.NET.UnitTests
 {
     [TestFixture]
     public class TranslatorFactoryTests
     {
-//        [Test]
-//        public void test_WHEN_expression_is_equal_THEN_eq_translator_is_created()
-//        {
-//            var tr = new TranslatorFactory().Create(ExpressionType.Equal);
-//            Assert.That(tr, Is.InstanceOf(typeof(EqTranslator)));
-//        }
+        [Test]
+        public void test_WHEN_eq_expression_THEN_generic_translator_is_created()
+        {
+            // arrange
+            var analyzerFactory = MockRepository.GenerateStub<IAnalyzerFactory>();
+            analyzerFactory.Stub(f => f.Create(ExpressionType.Equal)).Return(null);
+
+            // act
+            var tr = new TranslatorFactory(analyzerFactory).Create(ExpressionType.Equal);
+
+            // assert
+            Assert.That(tr, Is.InstanceOf(typeof(GenericTranslator)));
+        }
     }
 }
