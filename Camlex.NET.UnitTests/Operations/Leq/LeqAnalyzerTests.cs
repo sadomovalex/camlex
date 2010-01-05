4edc9b718aee2e3c2b3f91fcea557fb9f24de422
Alexey Sadomov <sadomovalex@camlex-online.org>
2010-01-05 14:50:56 +0000

    Lt (<) operation implemented

---
+++ Camlex.NET.UnitTests/Operations/Leq/LeqAnalyzerTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq.Expressions;
+using Camlex.NET.Impl.Operations.Gt;
+using Camlex.NET.Impl.Operations.Leq;
+using Camlex.NET.Interfaces;
+using Microsoft.SharePoint;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace Camlex.NET.UnitTests.Operations.Leq
+{
+    [TestFixture]
+    public class LeqAnalyzerTests
+    {
+        [Test]
+        public void test_THAT_leq_expression_IS_valid()
+        {
+            var analyzer = new LeqAnalyzer(null);
+            Expression<Func<SPItem, bool>> expr = x => (int) x["Count"] <= 1;
+            Assert.That(analyzer.IsValid(expr), Is.True);
+        }
+
+        [Test]
+        public void test_THAT_leq_expression_IS_determined_properly()
+        {
+            // arrange
+            Expression<Func<SPItem, bool>> expr = x => (int)x["Count"] <= 1;
+
+            var operandBuilder = MockRepository.GenerateStub<IOperandBuilder>();
+            operandBuilder.Stub(b => b.CreateFieldRefOperand(expr.Body)).Return(null);
+            operandBuilder.Stub(b => b.CreateValueOperand(expr.Body)).Return(null);
+
+            var analyzer = new LeqAnalyzer(operandBuilder);
+
+            // act
+            var operation = analyzer.GetOperation(expr);
+
+            //assert
+            Assert.That(operation, Is.InstanceOf<LeqOperation>());
+        }
+    }
+}
+
+
