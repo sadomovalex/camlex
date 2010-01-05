eeb598dfa88d41dd131016e078846d8e5d2dd38e
vtimashkov <vtimashkov@localhost>
2010-01-05 19:33:09 +0000

    Added some tests for Array operation

---
+++ Camlex.NET.UnitTests/Operations/Array/ArrayAnalyzerTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Camlex.NET.Impl.Operations.Array;
+using Camlex.NET.Interfaces;
+using Microsoft.SharePoint;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace Camlex.NET.UnitTests.Array
+{
+    [TestFixture]
+    public class ArrayAnalyzerTest
+    {
+        [Test]
+        public void test_THAT_array_expression_IS_valid()
+        {
+            var analyzer = new ArrayAnalyzer(null);
+            Expression<Func<SPItem, object[]>> expr = (x => new [] { x["field1"], x["field2"] as Camlex.Asc, x["field3"] as Camlex.Desc });
+            Assert.That(analyzer.IsValid(expr), Is.True);
+        }
+
+        [Test]
+        public void test_THAT_array_expression_IS_determined_properly()
+        {
+            // arrange
+            Expression<Func<SPItem, object[]>> expr = (x => new[] { x["field1"], x["field2"] as Camlex.Asc, x["field3"] as Camlex.Desc });
+            var expressions = ((NewArrayExpression)expr.Body).Expressions;
+
+            var operandBuilder = MockRepository.GenerateStub<IOperandBuilder>();
+            operandBuilder.Stub(b => b.CreateFieldRefOperandWithOrdering(expressions[0], new Camlex.Asc())).Return(null);
+            operandBuilder.Stub(b => b.CreateFieldRefOperandWithOrdering(((UnaryExpression)expressions[1]).Operand, new Camlex.Desc())).Return(null);
+            operandBuilder.Stub(b => b.CreateFieldRefOperandWithOrdering(((UnaryExpression)expressions[2]).Operand, new Camlex.Desc())).Return(null);
+            var analyzer = new ArrayAnalyzer(operandBuilder);
+
+            // act
+            var operation = analyzer.GetOperation(expr);
+
+            //assert
+            Assert.That(operation, Is.InstanceOf<ArrayOperation>());
+        }
+    }
+}
---
+++ Camlex.NET.UnitTests/Operations/Array/ArrayOperationTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Camlex.NET.Impl.Operations.Eq;
+using Camlex.NET.Impl.Operands;
+using Camlex.NET.UnitTests.Helpers;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace Camlex.NET.UnitTests.Array
+{
+    [TestFixture]
+    public class ArrayOperationTest
+    {
+        [Test]
+        public void test_THAT_operation_IS_rendered_to_caml_properly()
+        {
+//            // arrange
+//            var fieldRefOperandStub = MockRepository.GenerateStub<FieldRefOperand>("");
+//            var valueOperandStub = MockRepository.GenerateStub<IntegerValueOperand>(0);
+
+//            fieldRefOperandStub.Stub(o => o.ToCaml()).Return(new XElement("fieldRefOperandStub"));
+//            valueOperandStub.Stub(o => o.ToCaml()).Return(new XElement("valueOperandStub"));
+
+//            var operation = new EqOperation(fieldRefOperandStub, valueOperandStub);
+
+//            // act
+//            string caml = operation.ToCaml().ToString();
+
+//            // assert
+//            string expected =
+//                @"<Eq>
+//                    <fieldRefOperandStub />
+//                    <valueOperandStub />
+//                </Eq>";
+//            Assert.That(caml, Is.EqualTo(expected).Using(new CamlComparer()));
+        }
+    }
+}
