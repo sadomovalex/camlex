6d722cd464166047d815659b20c030fc6d521fe6
Alexey Sadomov <sadomovalex@camlex-online.org>
2010-01-07 15:27:42 +0000

    More tests

--- Camlex.NET.UnitTests/Operations/Eq/EqAnalyzerTests.cs
+++ Camlex.NET.UnitTests/Operations/Eq/EqAnalyzerTests.cs
@@ -45,6 +45,25 @@ namespace Camlex.NET.UnitTests.Operations.Eq
             //assert
             Assert.That(operation, Is.InstanceOf<EqOperation>());
         }
+
+        [Test]
+        public void test_THAT_string_based_eq_expression_IS_determined_properly()
+        {
+            // arrange
+            Expression<Func<SPItem, bool>> expr = x => x["Title"] == (DataTypes.Text)"testValue";
+
+            var operandBuilder = MockRepository.GenerateStub<IOperandBuilder>();
+            operandBuilder.Stub(b => b.CreateFieldRefOperand(expr.Body)).Return(null);
+            operandBuilder.Stub(b => b.CreateValueOperand(expr.Body)).Return(null);
+
+            var analyzer = new EqAnalyzer(null, operandBuilder);
+
+            // act
+            var operation = analyzer.GetOperation(expr);
+
+            //assert
+            Assert.That(operation, Is.InstanceOf<EqOperation>());
+        }
     }
 }
 
