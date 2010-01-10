d20ce10196cac8a848bd5cd9a97b472d27946779
vtimashkov <vtimashkov@gmail.com>
2010-01-10 10:58:02 +0000

    Added BeginsWith/Conatins analyzers/operations. Exntended ReflectionHelper/Tags to include BeginsWith/Contains items.

---
+++ Camlex.NET/Impl/Operations/Contains/ContainsOperation.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Linq;
+using Camlex.NET.Interfaces;
+
+namespace Camlex.NET.Impl.Operations.Contains
+{
+    public class ContainsOperation : BinaryOperationBase
+    {
+        public ContainsOperation(IOperationResultBuilder operationResultBuilder,
+            IOperand fieldRefOperand, IOperand valueOperand)
+            : base(operationResultBuilder, fieldRefOperand, valueOperand)
+        {
+        }
+
+        public override IOperationResult ToResult()
+        {
+            var result = new XElement(Tags.Eq,
+                             fieldRefOperand.ToCaml(),
+                             valueOperand.ToCaml());
+            return operationResultBuilder.CreateResult(result);
+        }
+    }
+}
