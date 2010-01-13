34aaec1ac93e4a79c3cc437afdd8e731c9f8e29a
vtimashkov <vtimashkov@localhost>
2010-01-13 22:02:32 +0000

    Added support for Camlex.Now and Camlex.Today

--- Camlex.NET.ConsoleTest/Program.cs
+++ Camlex.NET.ConsoleTest/Program.cs
@@ -19,7 +19,9 @@ namespace CamlexNET.ConsoleTest
             //Camlex.Query().Where(x => (DateTime)x["Modified"] == new DateTime(2010, 1, 2, 3, 4, 5));
             //Camlex.Query().Where(x => (DateTime)x["Modified"] == new DateTime(2010, 1, 2, 3, 4, 5).IncludeTimeValue());
             //Camlex.Query().Where(x => x["Modified"] == ((DataTypes.DateTime)"02.01.2010 03:04:05"));
-            Camlex.Query().Where(x => x["Modified"] == ((DataTypes.DateTime)"02.01.2010 03:04:05").IncludeTimeValue());
+            //Camlex.Query().Where(x => x["Modified"] == ((DataTypes.DateTime)"02.01.2010 03:04:05").IncludeTimeValue());
+            Camlex.Query().Where(x => x["Modified"] == ((DataTypes.DateTime)Camlex.Now));
+            Camlex.Query().Where(x => x["Modified"] == ((DataTypes.DateTime)Camlex.Today));
 
             //var param = "foo";
             //Camlex.Query().Where(x => ((string)x["Count"]).Contains(param));
@@ -29,7 +31,7 @@ namespace CamlexNET.ConsoleTest
             //Camlex.Query().Where(x => ((DataTypes.Text)x["Count"]).StartsWith(param));
             //Camlex.Query().Where(x => ((DataTypes.Note)x["Count"]).StartsWith("foo"));
 
-            Camlex.Query().OrderBy(x => x["field1"] as Camlex.Asc);
+            //Camlex.Query().OrderBy(x => x["field1"] as Camlex.Asc);
             //Camlex.Query().GroupBy(x => new[] { x["field1"], x["field2"] as Camlex.Asc, x["field3"] as Camlex.Desc }, true, 10);
             //StrongNamePublicKeyBlob myKey = GetSigningKey(typeof(Program));
         }
