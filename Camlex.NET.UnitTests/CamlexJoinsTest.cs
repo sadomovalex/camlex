4446f288a9cd0a59ccaa40a892591b7f8dd0b5e7
sadomovalex <sadomovalex@localhost>
2010-03-31 17:15:09 +0000

    Reaarange tests

---
+++ Camlex.NET.UnitTests/CamlexJoinsTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CamlexNET.UnitTests.Helpers;
+using NUnit.Framework;
+
+namespace CamlexNET.UnitTests
+{
+    [TestFixture]
+    public class CamlexJoinsTest
+    {
+        [Test]
+        public void test_THAT_2_eq_expression_with_andalso_ARE_translated_sucessfully()
+        {
+            string caml = Camlex.Query().Where(x => (string)x["Title"] == "testValue" &&
+                                            (int)x["Count"] == 1).ToString();
+
+            string expected =
+                //               "<Query>" +
+               "    <Where>" +
+               "        <And>" +
+               "            <Eq>" +
+               "                <FieldRef Name=\"Title\" />" +
+               "                <Value Type=\"Text\">testValue</Value>" +
+               "            </Eq>" +
+               "            <Eq>" +
+               "                <FieldRef Name=\"Count\" />" +
+               "                <Value Type=\"Integer\">1</Value>" +
+               "            </Eq>" +
+               "        </And>" +
+               "    </Where>";
+            //               "</Query>";
+
+            Assert.That(caml, Is.EqualTo(expected).Using(new CamlComparer()));
+        }
+
+        [Test]
+        public void test_THAT_expression_with_2_andalso_and_1_orelse_ARE_translated_sucessfully()
+        {
+            string caml = Camlex.Query().Where(x => ((string)x["Title"] == "testValue" &&
+                                            (int)x["Count1"] == 1) || (int)x["Count2"] == 2).ToString();
+
+            string expected =
+                //                "<Query>" +
+                "   <Where>" +
+                "       <Or>" +
+                "           <And>" +
+                "               <Eq>" +
+                "                   <FieldRef Name=\"Title\" />" +
+                "                   <Value Type=\"Text\">testValue</Value>" +
+                "               </Eq>" +
+                "               <Eq>" +
+                "                   <FieldRef Name=\"Count1\" />" +
+                "                   <Value Type=\"Integer\">1</Value>" +
+                "               </Eq>" +
+                "           </And>" +
+                "           <Eq>" +
+                "               <FieldRef Name=\"Count2\" />" +
+                "               <Value Type=\"Integer\">2</Value>" +
+                "           </Eq>" +
+                "       </Or>" +
+                "   </Where>";
+            //                "</Query>";
+
+            Assert.That(caml, Is.EqualTo(expected).Using(new CamlComparer()));
+        }
+
+    }
+}
