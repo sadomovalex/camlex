6ca9ac3d214f8be0e286926c173911de54bd2177
Alexey Sadomov <sadomovalex@camlex-online.org>
2011-04-07 20:43:25 +0300

    * New reverse_engeneering branch * Create ExpressionBuilder

---
+++ Camlex.NET/ReverseEngeneering/ExpressionBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Microsoft.SharePoint;
+
+namespace CamlexNET.ReverseEngeneering
+{
+    public static class ExpressionBuilder
+    {
+        public static Expression<Func<SPListItem, bool>> BuildFromXml(string xml)
+        {
+            if (string.IsNullOrEmpty(xml) || string.IsNullOrEmpty(xml.Trim()))
+            {
+                throw new ArgumentNullException("xml");
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
