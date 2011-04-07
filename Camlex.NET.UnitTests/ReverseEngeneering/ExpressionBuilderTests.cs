c48a640c91124116a7bb7cf2b0c4843362b45afa
sadomovalex <sadomovalex@localhost>
2011-04-07 20:43:25 +0300

    * New reverse_engeneering branch * Create ExpressionBuilder

---
+++ Camlex.NET.UnitTests/ReverseEngeneering/ExpressionBuilderTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CamlexNET.ReverseEngeneering;
+using NUnit.Framework;
+
+namespace CamlexNET.UnitTests.ReverseEngeneering
+{
+    [TestFixture]
+    public class ExpressionBuilderTests
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void test_WHEN_argument_is_null_THEN_exception_is_thrown()
+        {
+            ExpressionBuilder.BuildFromXml(null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void test_WHEN_argument_is_empty_THEN_exception_is_thrown()
+        {
+            ExpressionBuilder.BuildFromXml("");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void test_WHEN_argument_is_space_THEN_exception_is_thrown()
+        {
+            ExpressionBuilder.BuildFromXml(" ");
+        }
+    }
+}
