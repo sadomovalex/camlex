de423c1b1a3a0a41f861a4019aa571a6a316e60e
Alexey Sadomov <sadomovalex@camlex-online.org>
2011-04-08 23:20:45 +0300

    BooleanValueOperand reverse

---
+++ Camlex.NET.UnitTests/ReverseEngeneering/Operands/BooleanValueOperandTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CamlexNET.Impl.Operands;
+using CamlexNET.UnitTests.Helpers;
+using NUnit.Framework;
+
+namespace CamlexNET.UnitTests.ReverseEngeneering.Operands
+{
+    [TestFixture]
+    public class BooleanValueOperandTests
+    {
+        [Test]
+        public void test_THAT_operand_with_true_IS_conveted_to_expression_correctly()
+        {
+            var op = new BooleanValueOperand(true);
+            var expr = op.ToExpression();
+            Assert.That("true", Is.EqualTo(expr.ToString()).Using(new CaseInsensetiveComparer()));
+        }
+
+        [Test]
+        public void test_THAT_operand_with_false_IS_conveted_to_expression_correctly()
+        {
+            var op = new BooleanValueOperand(false);
+            var expr = op.ToExpression();
+            Assert.That("false", Is.EqualTo(expr.ToString()).Using(new CaseInsensetiveComparer()));
+        }
+    }
+}
