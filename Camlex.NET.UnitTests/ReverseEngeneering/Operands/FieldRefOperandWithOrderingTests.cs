f533150243492dfd6cfe2cd2f23119d744b6a40e
sadomovalex <sadomovalex@localhost>
2011-04-18 21:46:12 +0300

    FieldRefOperandWithOrdering reverse

---
+++ Camlex.NET.UnitTests/ReverseEngeneering/Operands/FieldRefOperandWithOrderingTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CamlexNET.Impl.Operands;
+using NUnit.Framework;
+
+namespace CamlexNET.UnitTests.ReverseEngeneering.Operands
+{
+    [TestFixture]
+    public class FieldRefOperandWithOrderingTests
+    {
+        [Test]
+        public void test_THAT_field_ref_operand_with_asc_IS_converted_to_expression_correctly()
+        {
+            var op = new FieldRefOperand("Title");
+            var opOrder = new FieldRefOperandWithOrdering(op, new Camlex.Asc());
+            var expr = opOrder.ToExpression();
+            Assert.That(expr.ToString(), Is.EqualTo("(x.get_Item(\"Title\") As Asc)"));
+            // problem 1 - Asc is not accessible. Only Camlex.Asc can be used
+            // problem 2 - As instead of as is used. It won't be compiled
+        }
+
+        [Test]
+        public void test_THAT_field_ref_operand_with_desc_IS_converted_to_expression_correctly()
+        {
+            var op = new FieldRefOperand("Title");
+            var opOrder = new FieldRefOperandWithOrdering(op, new Camlex.Desc());
+            var expr = opOrder.ToExpression();
+            Assert.That(expr.ToString(), Is.EqualTo("(x.get_Item(\"Title\") As Desc)"));
+        }
+
+        [Test]
+        public void test_THAT_field_ref_operand_with_none_IS_converted_to_expression_correctly()
+        {
+            var op = new FieldRefOperand("Title");
+            var opOrder = new FieldRefOperandWithOrdering(op, new Camlex.None());
+            var expr = opOrder.ToExpression();
+            Assert.That(expr.ToString(), Is.EqualTo("x.get_Item(\"Title\")"));
+        }
+    }
+}
