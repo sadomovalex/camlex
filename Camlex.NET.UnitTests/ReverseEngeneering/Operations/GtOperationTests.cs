053370833ae59361ecb038281eba82142e8d29da
sadomovalex <sadomovalex@localhost>
2011-04-29 23:26:04 +0300

    GtOperation reverse

--- Camlex.NET.UnitTests/ReverseEngeneering/Operations/GtOperationTests.cs
+++ Camlex.NET.UnitTests/ReverseEngeneering/Operations/GtOperationTests.cs
@@ -2,21 +2,22 @@
 using CamlexNET.Impl.Operands;
 using CamlexNET.Impl.Operations.Eq;
 using CamlexNET.Impl.Operations.Geq;
+using CamlexNET.Impl.Operations.Gt;
 using NUnit.Framework;
 
 namespace CamlexNET.UnitTests.ReverseEngeneering.Operations
 {
     [TestFixture]
-    public class GeqOperationTests
+    public class GtOperationTests
     {
         [Test]
-        public void test_THAT_geq_operation_IS_converted_to_expression_correctly()
+        public void test_THAT_gt_operation_IS_converted_to_expression_correctly()
         {
             var op1 = new FieldRefOperand("Count");
             var op2 = new IntegerValueOperand(1);
-            var op = new GeqOperation(null, op1, op2);
+            var op = new GtOperation(null, op1, op2);
             var expr = op.ToExpression();
-            Assert.That(expr.ToString(), Is.EqualTo("(Convert(x.get_Item(\"Count\")) >= 1)"));
+            Assert.That(expr.ToString(), Is.EqualTo("(Convert(x.get_Item(\"Count\")) > 1)"));
         }
     }
 }
