6040df0327c54465c10d218ee8ec7cee04840ea5
sadomovalex <sadomovalex@localhost>
2011-05-05 22:51:26 +0300

    * NeqOperation reverse * OrElseOperation reverse

---
+++ Camlex.NET.UnitTests/ReverseEngeneering/Operations/OrElseOperationTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CamlexNET.Impl.Operands;
+using CamlexNET.Impl.Operations.AndAlso;
+using CamlexNET.Impl.Operations.Eq;
+using CamlexNET.Impl.Operations.OrElse;
+using NUnit.Framework;
+
+namespace CamlexNET.UnitTests.ReverseEngeneering.Operations
+{
+    [TestFixture]
+    public class OrElseOperationTests
+    {
+        [Test]
+        public void test_THAT_or_else_operation_IS_converted_to_expression_correctly()
+        {
+            var op11 = new FieldRefOperand("Status");
+            var op12 = new BooleanValueOperand(true);
+            var op1 = new EqOperation(null, op11, op12);
+
+            var op21 = new FieldRefOperand("Status");
+            var op22 = new BooleanValueOperand(false);
+            var op2 = new EqOperation(null, op21, op22);
+
+            var op = new OrElseOperation(null, op1, op2);
+
+            var expr = op.ToExpression();
+            Assert.That(expr.ToString(), Is.EqualTo("((Convert(x.get_Item(\"Status\")) = True) || (Convert(x.get_Item(\"Status\")) = False))"));
+        }
+    }
+}
--- Camlex.NET/Impl/Operations/Neq/NeqOperation.cs
+++ Camlex.NET/Impl/Operations/Neq/NeqOperation.cs
@@ -51,7 +51,10 @@ namespace CamlexNET.Impl.Operations.Neq
 
         public override Expression ToExpression()
         {
-            throw new NotImplementedException();
+            var fieldRef = this.getFieldRefOperandExpression();
+            var value = this.getValueOperandExpression();
+
+            return Expression.NotEqual(fieldRef, value);
         }
     }
 }
