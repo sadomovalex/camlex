b4264b4a7194748f0f85c1f3e922c6f1855b159a
sadomovalex <sadomovalex@localhost>
2011-08-06 00:24:34 +0300

    More tests

--- Camlex.NET.UnitTests/CamlexUserTests.cs
+++ Camlex.NET.UnitTests/CamlexUserTests.cs
@@ -13,13 +13,13 @@ namespace CamlexNET.UnitTests
         [Test]
         public void test_THAT_user_id_field_ref_with_name_IS_translated_successfully()
         {
-            string caml = Camlex.Query().Where(x => x["Ref"] == (DataTypes.LookupId)"123").ToString();
+            string caml = Camlex.Query().Where(x => x["foo"] == (DataTypes.UserId)"123").ToString();
 
             string expected =
                 "   <Where>" +
                 "       <Eq>" +
-                "           <FieldRef Name=\"Ref\" LookupId=\"True\" />" +
-                "           <Value Type=\"Lookup\">123</Value>" +
+                "           <FieldRef Name=\"foo\" LookupId=\"True\" />" +
+                "           <Value Type=\"User\">123</Value>" +
                 "       </Eq>" +
                 "   </Where>";
 
@@ -27,16 +27,16 @@ namespace CamlexNET.UnitTests
         }
 
         [Test]
-        public void test_THAT_lookup_id_field_ref_with_guid_IS_translated_successfully()
+        public void test_THAT_user_id_field_ref_with_guid_IS_translated_successfully()
         {
             var guid = new Guid("{4feaf1f3-5b04-4d93-b0fc-4e48d0c60eed}");
-            string caml = Camlex.Query().Where(x => x[guid] == (DataTypes.LookupId)"123").ToString();
+            string caml = Camlex.Query().Where(x => x[guid] == (DataTypes.UserId)"123").ToString();
 
             string expected =
                 "   <Where>" +
                 "       <Eq>" +
                 "           <FieldRef ID=\"4feaf1f3-5b04-4d93-b0fc-4e48d0c60eed\" LookupId=\"True\" />" +
-                "           <Value Type=\"Lookup\">123</Value>" +
+                "           <Value Type=\"User\">123</Value>" +
                 "       </Eq>" +
                 "   </Where>";
 
@@ -44,15 +44,15 @@ namespace CamlexNET.UnitTests
         }
 
         [Test]
-        public void test_THAT_lookup_value_IS_translated_successfully()
+        public void test_THAT_expression_with_user_IS_translated_successfully()
         {
-            string caml = Camlex.Query().Where(x => x["Ref"] == (DataTypes.LookupValue)"123").ToString();
+            string caml = Camlex.Query().Where(x => x["foo"] == (DataTypes.User)"Foo Bar").ToString();
 
             string expected =
                 "   <Where>" +
                 "       <Eq>" +
-                "           <FieldRef Name=\"Ref\" />" +
-                "           <Value Type=\"Lookup\">123</Value>" +
+                "           <FieldRef Name=\"foo\" />" +
+                "           <Value Type=\"User\">Foo Bar</Value>" +
                 "       </Eq>" +
                 "   </Where>";
 
