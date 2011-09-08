6eeb5d4ce46585bb43e01a2bbee2cd8b7820e029
sadomovalex <sadomovalex@localhost>
2011-09-09 00:04:28 +0300

    Re linker from CAML

--- Camlex.NET/Exceptions.cs
+++ Camlex.NET/Exceptions.cs
@@ -186,4 +186,20 @@ namespace CamlexNET
             : base(string.Format("Incorrect translator type was passed to the linker: '{0}'. Linker from CAML requires translator from CAML", type))
         {}
     }
+
+//    internal class CantParseBooleanAttributeException : Exception
+//    {
+//        public CantParseBooleanAttributeException(string attr) :
+//            base(string.Format("Can't parse boolean attribute '{0}'", attr))
+//        {
+//        }
+//    }
+//
+//    internal class CantParseIntegerAttributeException : Exception
+//    {
+//        public CantParseIntegerAttributeException(string attr) :
+//            base(string.Format("Can't parse integer attribute '{0}'", attr))
+//        {
+//        }
+//    }
 }
