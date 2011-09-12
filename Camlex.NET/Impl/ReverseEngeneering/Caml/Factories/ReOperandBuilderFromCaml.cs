68b2fababdbb098be7c23ef8bad934c1cc517dff
sadomovalex <sadomovalex@localhost>
2011-09-13 00:10:54 +0300

    * ReOperandBuilder * ReArrayAnalyzer

--- Camlex.NET/Impl/ReverseEngeneering/Caml/Factories/ReOperandBuilderFromCaml.cs
+++ Camlex.NET/Impl/ReverseEngeneering/Caml/Factories/ReOperandBuilderFromCaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Linq;
+using CamlexNET.Impl.Operands;
 using CamlexNET.Interfaces;
 using CamlexNET.Interfaces.ReverseEngeneering;
 
@@ -9,28 +11,74 @@ namespace CamlexNET.Impl.ReverseEngeneering.Caml.Factories
 {
     internal class ReOperandBuilderFromCaml : IReOperandBuilder
     {
-        public IOperand CreateFieldRefOperand(string input, IOperand valueOperand)
+        public IOperand CreateFieldRefOperand(XElement el, IOperand valueOperand)
         {
+            if (el == null)
+            {
+                throw new ArgumentNullException("el");
+            }
             throw new NotImplementedException();
         }
 
-        public IOperand CreateFieldRefOperandWithOrdering(string input, Camlex.OrderDirection orderDirection)
+        public IOperand CreateFieldRefOperandWithOrdering(XElement el, Camlex.OrderDirection orderDirection)
         {
-            throw new NotImplementedException();
+            if (el == null)
+            {
+                throw new ArgumentNullException("el");
+            }
+
+            Guid? id = null;
+            var idAttr = el.Attributes().FirstOrDefault(a => a.Name == Attributes.ID);
+            if (idAttr != null)
+            {
+                try
+                {
+                    id = new Guid(idAttr.Value);
+                }
+                catch
+                {
+                    throw new NotCorrectAttrValueException(idAttr.Value, Attributes.ID);
+                }
+            }
+
+            string name = null;
+            var nameAttr = el.Attributes().FirstOrDefault(a => a.Name == Attributes.Name);
+            if (nameAttr != null)
+            {
+                name = nameAttr.Value;
+            }
+
+            if (id != null && !string.IsNullOrEmpty(name))
+            {
+                throw new OnlyOneAttributeShouldBeSpecified(Attributes.ID, Attributes.Name);
+            }
+
+            if (id == null && string.IsNullOrEmpty(name))
+            {
+                throw new AtLeastOneAttributeShouldBeSpecified(Attributes.ID, Attributes.Name);
+            }
+
+            var attributes = el.Attributes().Where(
+                attr =>
+                {
+                    return (attr.Name != Attributes.ID && attr.Name != Attributes.Name &&
+                            !string.IsNullOrEmpty(attr.Value));
+                })
+                    .Select(attr => new KeyValuePair<string, string>(attr.Name.ToString(), attr.Value))
+                    .ToList();
+            if (id != null)
+            {
+                return new FieldRefOperand(id.Value, attributes);
+            }
+            return new FieldRefOperand(name, attributes);
         }
 
-        public IOperand CreateValueOperandForNativeSyntax(string input)
+        public IOperand CreateValueOperand(XElement el)
         {
-            throw new NotImplementedException();
-        }
-
-        public IOperand CreateValueOperandForNativeSyntax(string input, Type explicitOperandType)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IOperand CreateValueOperandForStringBasedSyntax(string input)
-        {
+            if (el == null)
+            {
+                throw new ArgumentNullException("el");
+            }
             throw new NotImplementedException();
         }
     }
