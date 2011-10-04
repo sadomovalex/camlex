91bff093f18eb7ff5a8fb37b1ce3188d6581cd55
Alexey Sadomov <sadomovalex@camlex-online.org>
2011-10-04 21:11:35 +0300

    More tests

--- Camlex.NET.ExpressionToCode.UnitTests/ExpressionToCodeAdapterTests.cs
+++ Camlex.NET.ExpressionToCode.UnitTests/ExpressionToCodeAdapterTests.cs
@@ -23,5 +23,22 @@ namespace CamlexNET.ExpressionToCode.UnitTests
             string code = Adapter.ToCode(expr);
             Assert.That(code, Is.EqualTo("Camlex.Query().Where(x => (string)x[\"Title\"] == \"testValue\")"));
         }
+
+        [Test]
+        public void test_THAT_single_eq_expression_with_string_variable_which_is_null_IS_translated_sucessfully()
+        {
+            string xml =
+                "<Query>" +
+                "   <Where>" +
+                "       <IsNull>" +
+                "           <FieldRef Name=\"Title\" />" +
+                "       </IsNull>" +
+                "   </Where>" +
+                "</Query>";
+
+            var expr = Camlex.QueryFromString(xml).ToExpression();
+            string code = Adapter.ToCode(expr);
+            Assert.That(code, Is.EqualTo("Camlex.Query().Where(x => x[\"Title\"] == null)"));
+        }
     }
 }
