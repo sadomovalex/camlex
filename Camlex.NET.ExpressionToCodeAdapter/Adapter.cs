ffad77a6dc47984cdb3409f2c36e6f64e96dfb23
Alexey Sadomov <sadomovalex@camlex-online.org>
2011-10-04 20:11:08 +0300

    Add ExpressionToCodeAdapter project

---
+++ Camlex.NET.ExpressionToCodeAdapter/Adapter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using ExpressionToCodeLib;
+
+namespace CamlexNET.ExpressionToCodeAdapter
+{
+    public static class Adapter
+    {
+        public static string ToCode(Expression expr)
+        {
+            if (expr == null)
+            {
+                return string.Empty;
+            }
+            string code = ExpressionToCode.ToCode(expr);
+            if (string.IsNullOrEmpty(code))
+            {
+                return string.Empty;
+            }
+            return code;
+        }
+    }
+}
