4a8083106c70b35c9729707104cb48259f9b9e8e
sadomovalex <sadomovalex@localhost>
2011-10-04 20:30:04 +0300

    ExpressionToCodeAdapter tests

---
+++ Camlex.NET.UnitTests/ExpressionToCode/ExpressionToCodeAdapterTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CamlexNET.ExpressionToCodeAdapter;
+using NUnit.Framework;
+
+namespace CamlexNET.UnitTests.ExpressionToCode
+{
+    [TestFixture]
+    public class ExpressionToCodeAdapterTests
+    {
+        [Test]
+        public void test_THAT_single_eq_expression_IS_translated_sucessfully()
+        {
+            string xml =
+                "<Query>" +
+                "   <Where>" +
+                "       <Eq>" +
+                "           <FieldRef Name=\"Title\" />" +
+                "           <Value Type=\"Text\">testValue</Value>" +
+                "       </Eq>" +
+                "   </Where>" +
+                "</Query>";
+
+            var expr = Camlex.QueryFromString(xml).ToExpression();
+            string code = Adapter.ToCode(expr);
+            Assert.That(code, Is.EqualTo(""));
+        }
+    }
+}
