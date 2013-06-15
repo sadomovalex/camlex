7bb3b4a122c784d256a0430b47c9e10bdab897f7
Alexey Sadomov <sadomovalex@camlex-online.org>
2013-06-15 12:07:12 +0300

    Reverse for In operation

---
+++ Camlex.NET.UnitTests/ReverseEngeneering/Operands/ValuesValueOperandTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CamlexNET.Impl.Operands;
+using CamlexNET.Interfaces;
+using NUnit.Framework;
+
+namespace CamlexNET.UnitTests.ReverseEngeneering.Operands
+{
+    [TestFixture]
+    public class ValuesValueOperandTests
+    {
+        [Test]
+        public void test_THAT_operand_with_several_operands_IS_conveted_to_expression_correctly()
+        {
+            var values = new List<IOperand>();
+            values.Add(new TextValueOperand("test1"));
+            values.Add(new TextValueOperand("test2"));
+
+            var op = new ValuesValueOperand(values);
+            var expr = op.ToExpression();
+            Assert.That(expr.ToString(), Is.EqualTo("new [] {\"test1\", \"test2\"}"));
+        }
+    }
+}
