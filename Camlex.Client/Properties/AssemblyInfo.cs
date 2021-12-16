069a39903be7181d6f4c47c9b67ecfca9fefac04
Alexey Sadomov <alexey.sadomov@valointranet.com>
2021-12-16 17:48:44 +0300

    Increase assembly version

--- Camlex.Client/Properties/AssemblyInfo.cs
+++ Camlex.Client/Properties/AssemblyInfo.cs
@@ -48,6 +48,8 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8819cd03-43e1-4a07-8f9f-853ef298edb6")]
 
+// NOTE: assembly version is not specified in this file anymore (so it is commented here). In order to specify assembly version
+// go to project settings > Package tab
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
